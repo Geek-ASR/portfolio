9d7395abcfcf54e5b3536339504f1473739736f9
Aditya Rekhe <adityarekhe1030@gmail.com>
2025-06-05 14:10:14 +0000

    not perfect. check the shades color in the attached image i want you to

--- src/components/ui/mountain-parallax-background.tsx
+++ src/components/ui/mountain-parallax-background.tsx
@@ -40,37 +40,37 @@ const MountainLayer: FC<MountainLayerProps> = ({
 const MountainParallaxBackground: FC = () => {
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden" aria-hidden="true">
-      {/* Layer 4: Furthest, deep sunset sky color */}
+      {/* Layer 4: Furthest, very dark upper sky */}
       <MountainLayer
         d="M0,400 L0,320 Q360,280 720,300 T1440,270 L1440,400 Z"
-        fillClass="fill-purple-800" // Deep purple/red for distant sunset sky
-        opacityClass="opacity-70"
+        fillClass="fill-slate-900" // Very dark blueish-gray, almost black
+        opacityClass="opacity-100"
         yOffsetRange={[0, -40]}
         heightClass="h-[calc(100vh+40px)]"
         viewBox="0 0 1440 400"
       />
-      {/* Layer 3: Rich red-orange */}
+      {/* Layer 3: Sunset glow band */}
       <MountainLayer
         d="M0,400 L0,300 L150,330 L300,280 C400,260 500,290 600,270 L750,310 L900,250 C1000,230 1100,280 1200,270 L1350,290 L1440,280 L1440,400 Z"
-        fillClass="fill-red-700" // Rich red-orange
-        opacityClass="opacity-80"
+        fillClass="fill-orange-700" // Dark, rich orange for sunset band
+        opacityClass="opacity-90"
         yOffsetRange={[0, -80]}
         heightClass="h-[calc(100vh+80px)]"
         viewBox="0 0 1440 400"
       />
-      {/* Layer 2: Bright orange */}
+      {/* Layer 2: Mid-ground silhouetted mountains */}
       <MountainLayer
         d="M0,400 L0,280 L200,350 L380,260 L550,340 L720,240 L900,330 L1050,250 L1200,320 L1350,270 L1440,300 L1440,400 Z"
-        fillClass="fill-orange-600" // Bright orange
-        opacityClass="opacity-90"
+        fillClass="fill-neutral-800" // Dark gray silhouette
+        opacityClass="opacity-100"
         yOffsetRange={[0, -130]}
         heightClass="h-[calc(100vh+130px)]"
         viewBox="0 0 1440 400"
       />
-      {/* Layer 1: Closest, bright yellow-orange */}
+      {/* Layer 1: Closest, darkest silhouetted mountains */}
       <MountainLayer
         d="M0,400 L0,250 L100,320 L250,220 L400,340 L580,180 L720,300 L850,200 L1000,330 L1150,230 L1300,310 L1440,260 L1440,400 Z"
-        fillClass="fill-yellow-500" // Brightest yellow-orange hitting the peaks
+        fillClass="fill-neutral-950" // Almost black silhouette
         opacityClass="opacity-100"
         yOffsetRange={[0, -200]}
         heightClass="h-[calc(100vh+200px)]"
