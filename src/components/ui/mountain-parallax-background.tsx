0ae8f3577dd3ccbe8f89ff4819f4d8e005cfa372
Aditya Rekhe <adityarekhe1030@gmail.com>
2025-06-05 14:40:33 +0000

    use this color - #dea670 as background sky color behind the mountain

--- src/components/ui/mountain-parallax-background.tsx
+++ src/components/ui/mountain-parallax-background.tsx
@@ -41,7 +41,7 @@ const MountainParallaxBackground: FC = () => {
   return (
     <div
       className="fixed inset-0 -z-10 overflow-hidden"
-      style={{ backgroundColor: '#d99759' }}
+      style={{ backgroundColor: '#dea670' }} // Updated background color
       aria-hidden="true"
     >
       {/* Layer 3: Distant, Hazy Mountains */}
