212eee6508de98c262790a6a6ef6738c9d0b7579
Aditya Rekhe <adityarekhe1030@gmail.com>
2025-06-05 14:49:02 +0000

    change the color of the mountain to :#735446

--- src/components/ui/mountain-parallax-background.tsx
+++ src/components/ui/mountain-parallax-background.tsx
@@ -6,7 +6,7 @@ import type { FC } from 'react';
 
 interface MountainLayerProps {
   d: string;
-  fillClass: string;
+  fillColor: string; // Changed from fillClass to fillColor
   opacityClass?: string;
   yOffsetRange: [number, number];
   heightClass: string;
@@ -15,7 +15,7 @@ interface MountainLayerProps {
 
 const MountainLayer: FC<MountainLayerProps> = ({
   d,
-  fillClass,
+  fillColor, // Use fillColor
   opacityClass = 'opacity-100',
   yOffsetRange,
   heightClass,
@@ -29,10 +29,12 @@ const MountainLayer: FC<MountainLayerProps> = ({
       style={{ y }}
       viewBox={viewBox}
       preserveAspectRatio="xMidYMid slice"
-      className={`absolute inset-x-0 top-0 w-full ${heightClass} ${fillClass} ${opacityClass}`}
+      // Removed fillClass from here, it's now applied to the path
+      className={`absolute inset-x-0 top-0 w-full ${heightClass} ${opacityClass}`}
       aria-hidden="true"
     >
-      <path d={d} />
+      {/* Apply fillColor directly to the path */}
+      <path d={d} style={{ fill: fillColor }} />
     </motion.svg>
   );
 };
@@ -41,13 +43,13 @@ const MountainParallaxBackground: FC = () => {
   return (
     <div
       className="fixed inset-0 -z-10 overflow-hidden"
-      style={{ backgroundColor: '#eed1b5' }} // Updated background color
+      style={{ backgroundColor: '#eed1b5' }}
       aria-hidden="true"
     >
-      {/* Layer 3: Distant, Hazy Mountains */}
+      {/* Layer 3: Distant Mountains */}
       <MountainLayer
         d="M0,280 Q180,220 360,250 T720,230 Q900,210 1080,240 T1440,220 L1440,400 L0,400 Z"
-        fillClass="fill-slate-600"
+        fillColor="#735446" // Updated color
         opacityClass="opacity-85"
         yOffsetRange={[0, -80]}
         heightClass="h-[calc(100vh+80px)]"
@@ -56,7 +58,7 @@ const MountainParallaxBackground: FC = () => {
       {/* Layer 2: Mid-Ground Mountains */}
       <MountainLayer
         d="M0,320 Q150,250 300,280 Q450,220 600,270 Q750,200 900,260 Q1050,230 1200,280 L1440,250 L1440,400 L0,400 Z"
-        fillClass="fill-slate-700"
+        fillColor="#735446" // Updated color
         opacityClass="opacity-95"
         yOffsetRange={[0, -130]}
         heightClass="h-[calc(100vh+130px)]"
@@ -65,7 +67,7 @@ const MountainParallaxBackground: FC = () => {
       {/* Layer 1: Foreground Mountains */}
       <MountainLayer
         d="M0,400 L0,350 Q120,280 240,310 Q360,240 480,300 Q600,220 720,290 Q840,250 960,320 Q1080,260 1200,310 L1320,280 L1440,340 L1440,400 Z"
-        fillClass="fill-neutral-800" // Using a darker neutral for strong silhouette
+        fillColor="#735446" // Updated color
         opacityClass="opacity-100"
         yOffsetRange={[0, -200]}
         heightClass="h-[calc(100vh+200px)]"
