a7a0d0afce82be5f2ef5822a9a5c6f3f38470e2b
Aditya Rekhe <adityarekhe1030@gmail.com>
2025-06-05 13:39:39 +0000

    when i scroll i dont want that the layers end(bottom) should go up and i

--- src/components/ui/mountain-parallax-background.tsx
+++ src/components/ui/mountain-parallax-background.tsx
@@ -9,7 +9,7 @@ interface MountainLayerProps {
   fillClass: string;
   opacityClass?: string;
   yOffsetRange: [number, number];
-  heightClass: string;
+  heightClass: string; // Will now be a calculated class like h-[calc(100vh+Xpx)]
   viewBox?: string;
 }
 
@@ -29,7 +29,8 @@ const MountainLayer: FC<MountainLayerProps> = ({
       style={{ y }}
       viewBox={viewBox}
       preserveAspectRatio="xMidYMid slice"
-      className={`absolute inset-x-0 bottom-0 w-full ${heightClass} ${fillClass} ${opacityClass}`}
+      // Changed from `absolute inset-x-0 bottom-0` to `absolute inset-x-0 top-0`
+      className={`absolute inset-x-0 top-0 w-full ${heightClass} ${fillClass} ${opacityClass}`}
       aria-hidden="true"
     >
       <path d={d} />
@@ -43,37 +44,37 @@ const MountainParallaxBackground: FC = () => {
       {/* Layer 4: Furthest, darkest, smooth curves for haze */}
       <MountainLayer
         d="M0,400 L0,320 Q360,280 720,300 T1440,270 L1440,400 Z"
-        fillClass="fill-blue-900" // Darkest
-        opacityClass="opacity-75" // Was opacity-100 - More transparent for distance
+        fillClass="fill-blue-900"
+        opacityClass="opacity-75"
         yOffsetRange={[0, -40]}
-        heightClass="h-[65vh]"
+        heightClass="h-[calc(100vh+40px)]" // Adjusted height
         viewBox="0 0 1440 400"
       />
       {/* Layer 3: Slightly more defined, still distant */}
       <MountainLayer
         d="M0,400 L0,300 L150,330 L300,280 C400,260 500,290 600,270 L750,310 L900,250 C1000,230 1100,280 1200,270 L1350,290 L1440,280 L1440,400 Z"
         fillClass="fill-blue-700"
-        opacityClass="opacity-85" // Was opacity-90
+        opacityClass="opacity-85"
         yOffsetRange={[0, -80]}
-        heightClass="h-[75vh]"
+        heightClass="h-[calc(100vh+80px)]" // Adjusted height
         viewBox="0 0 1440 400"
       />
       {/* Layer 2: More prominent, sharper peaks */}
       <MountainLayer
         d="M0,400 L0,280 L200,350 L380,260 L550,340 L720,240 L900,330 L1050,250 L1200,320 L1350,270 L1440,300 L1440,400 Z"
         fillClass="fill-blue-600"
-        opacityClass="opacity-95" // Was opacity-85
+        opacityClass="opacity-95"
         yOffsetRange={[0, -130]}
-        heightClass="h-[85vh]"
+        heightClass="h-[calc(100vh+130px)]" // Adjusted height
         viewBox="0 0 1440 400"
       />
       {/* Layer 1: Closest, most detailed, highest peaks */}
       <MountainLayer
         d="M0,400 L0,250 L100,320 L250,220 L400,340 L580,180 L720,300 L850,200 L1000,330 L1150,230 L1300,310 L1440,260 L1440,400 Z"
-        fillClass="fill-blue-500" // Lightest
-        opacityClass="opacity-100" // Was opacity-80 - Fully opaque for closest
+        fillClass="fill-blue-500"
+        opacityClass="opacity-100"
         yOffsetRange={[0, -200]}
-        heightClass="h-full"
+        heightClass="h-[calc(100vh+200px)]" // Adjusted height
         viewBox="0 0 1440 400"
       />
     </div>
