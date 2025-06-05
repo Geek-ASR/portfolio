2edc2f6e0d53c5b9f29bcefd420fea1c171483b9
Aditya Rekhe <adityarekhe1030@gmail.com>
2025-06-05 14:30:45 +0000

    it doesnt even look like mountains now. make it look like mountain. remo

--- src/components/ui/mountain-parallax-background.tsx
+++ src/components/ui/mountain-parallax-background.tsx
@@ -19,7 +19,7 @@ const MountainLayer: FC<MountainLayerProps> = ({
   opacityClass = 'opacity-100',
   yOffsetRange,
   heightClass,
-  viewBox = "0 0 1440 400", 
+  viewBox = "0 0 1440 400",
 }) => {
   const { scrollYProgress } = useScroll();
   const y = useTransform(scrollYProgress, [0, 1], yOffsetRange);
@@ -39,50 +39,32 @@ const MountainLayer: FC<MountainLayerProps> = ({
 
 const MountainParallaxBackground: FC = () => {
   return (
-    <div className="fixed inset-0 -z-10 overflow-hidden" aria-hidden="true">
-      {/* Layer 5: Deep Upper Sky (Furthest) */}
+    <div className="fixed inset-0 -z-10 overflow-hidden bg-sky-200" aria-hidden="true">
+      {/* Layer 3: Distant, Hazy Mountains */}
       <MountainLayer
-        d="M0,150 Q360,50 720,100 T1440,80 L1440,400 L0,400 Z"
-        fillClass="fill-gray-900"
-        opacityClass="opacity-100"
-        yOffsetRange={[0, -30]}
-        heightClass="h-[calc(100vh+30px)]"
-        viewBox="0 0 1440 400"
-      />
-      {/* Layer 4: Sunset Glow Band */}
-      <MountainLayer
-        d="M0,200 Q360,180 720,190 T1440,170 L1440,280 Q720,300 0,270 Z"
-        fillClass="fill-red-600"
-        opacityClass="opacity-90"
-        yOffsetRange={[0, -60]}
-        heightClass="h-[calc(100vh+60px)]"
-        viewBox="0 0 1440 400"
-      />
-      {/* Layer 3: Distant, Hazy Mountains (More compressed and higher base) */}
-      <MountainLayer
-        d="M0,400 L0,280 Q150,270 300,285 Q450,300 600,280 Q750,260 900,275 Q1050,290 1200,270 L1440,290 L1440,400 Z"
-        fillClass="fill-slate-700"
+        d="M0,280 Q180,220 360,250 T720,230 Q900,210 1080,240 T1440,220 L1440,400 L0,400 Z"
+        fillClass="fill-slate-600"
         opacityClass="opacity-85"
-        yOffsetRange={[0, -100]}
-        heightClass="h-[calc(100vh+100px)]"
+        yOffsetRange={[0, -80]}
+        heightClass="h-[calc(100vh+80px)]"
         viewBox="0 0 1440 400"
       />
-      {/* Layer 2: Mid-Ground Mountains (More compressed, starts lower) */}
+      {/* Layer 2: Mid-Ground Mountains */}
       <MountainLayer
-        d="M0,400 L0,310 Q200,300 400,315 Q600,330 800,310 Q1000,290 1200,305 L1440,320 L1440,400 Z"
-        fillClass="fill-neutral-800"
-        opacityClass="opacity-100"
-        yOffsetRange={[0, -150]}
-        heightClass="h-[calc(100vh+150px)]"
+        d="M0,320 Q150,250 300,280 Q450,220 600,270 Q750,200 900,260 Q1050,230 1200,280 L1440,250 L1440,400 L0,400 Z"
+        fillClass="fill-slate-700"
+        opacityClass="opacity-95"
+        yOffsetRange={[0, -130]}
+        heightClass="h-[calc(100vh+130px)]"
         viewBox="0 0 1440 400"
       />
-      {/* Layer 1: Foreground Mountains (Closest, more prominent peaks, starts lower) */}
+      {/* Layer 1: Foreground Mountains */}
       <MountainLayer
-        d="M-5,400 L-5,340 C150,330 300,350 450,340 C600,330 750,360 900,350 C1050,340 1200,370 1350,360 L1445,350 L1445,400 Z"
-        fillClass="fill-black"
+        d="M0,400 L0,350 Q120,280 240,310 Q360,240 480,300 Q600,220 720,290 Q840,250 960,320 Q1080,260 1200,310 L1320,280 L1440,340 L1440,400 Z"
+        fillClass="fill-neutral-800" // Using a darker neutral for strong silhouette
         opacityClass="opacity-100"
-        yOffsetRange={[0, -220]}
-        heightClass="h-[calc(100vh+220px)]"
+        yOffsetRange={[0, -200]}
+        heightClass="h-[calc(100vh+200px)]"
         viewBox="0 0 1440 400"
       />
     </div>
