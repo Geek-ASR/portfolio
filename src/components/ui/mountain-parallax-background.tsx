531fc7d2e5a10d53f4c63c575e052ce219a91526
Aditya Rekhe <adityarekhe1030@gmail.com>
2025-06-05 14:21:06 +0000

    add more layers to it so it can match the image that i have attached the

--- src/components/ui/mountain-parallax-background.tsx
+++ src/components/ui/mountain-parallax-background.tsx
@@ -19,7 +19,7 @@ const MountainLayer: FC<MountainLayerProps> = ({
   opacityClass = 'opacity-100',
   yOffsetRange,
   heightClass,
-  viewBox = "0 0 1440 400",
+  viewBox = "0 0 1440 400", // Standard viewBox, content will scale
 }) => {
   const { scrollYProgress } = useScroll();
   const y = useTransform(scrollYProgress, [0, 1], yOffsetRange);
@@ -28,7 +28,7 @@ const MountainLayer: FC<MountainLayerProps> = ({
     <motion.svg
       style={{ y }}
       viewBox={viewBox}
-      preserveAspectRatio="xMidYMid slice"
+      preserveAspectRatio="xMidYMid slice" // Ensures the SVG fills the container and maintains aspect ratio
       className={`absolute inset-x-0 top-0 w-full ${heightClass} ${fillClass} ${opacityClass}`}
       aria-hidden="true"
     >
@@ -40,40 +40,49 @@ const MountainLayer: FC<MountainLayerProps> = ({
 const MountainParallaxBackground: FC = () => {
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden" aria-hidden="true">
-      {/* Layer 4: Furthest, very dark upper sky */}
+      {/* Layer 5: Deep Upper Sky (Furthest) */}
       <MountainLayer
-        d="M0,400 L0,320 Q360,280 720,300 T1440,270 L1440,400 Z"
-        fillClass="fill-slate-900" // Very dark blueish-gray, almost black
+        d="M0,150 Q360,50 720,100 T1440,80 L1440,400 L0,400 Z" // High arching sky
+        fillClass="fill-gray-900"
         opacityClass="opacity-100"
-        yOffsetRange={[0, -40]}
-        heightClass="h-[calc(100vh+40px)]"
+        yOffsetRange={[0, -30]}
+        heightClass="h-[calc(100vh+30px)]"
         viewBox="0 0 1440 400"
       />
-      {/* Layer 3: Sunset glow band */}
+      {/* Layer 4: Sunset Glow Band */}
       <MountainLayer
-        d="M0,400 L0,300 L150,330 L300,280 C400,260 500,290 600,270 L750,310 L900,250 C1000,230 1100,280 1200,270 L1350,290 L1440,280 L1440,400 Z"
-        fillClass="fill-orange-700" // Dark, rich orange for sunset band
+        d="M0,200 Q360,180 720,190 T1440,170 L1440,280 Q720,300 0,270 Z" // Glow band, slightly uneven
+        fillClass="fill-red-600"
         opacityClass="opacity-90"
-        yOffsetRange={[0, -80]}
-        heightClass="h-[calc(100vh+80px)]"
+        yOffsetRange={[0, -60]}
+        heightClass="h-[calc(100vh+60px)]"
         viewBox="0 0 1440 400"
       />
-      {/* Layer 2: Mid-ground silhouetted mountains */}
+      {/* Layer 3: Distant, Hazy Mountains */}
       <MountainLayer
-        d="M0,400 L0,280 L200,350 L380,260 L550,340 L720,240 L900,330 L1050,250 L1200,320 L1350,270 L1440,300 L1440,400 Z"
-        fillClass="fill-neutral-800" // Dark gray silhouette
-        opacityClass="opacity-100"
-        yOffsetRange={[0, -130]}
-        heightClass="h-[calc(100vh+130px)]"
+        d="M0,400 L0,280 C100,260 250,300 400,270 C550,240 650,280 720,260 C800,240 950,290 1100,250 L1250,280 L1440,240 L1440,400 Z"
+        fillClass="fill-slate-700"
+        opacityClass="opacity-85"
+        yOffsetRange={[0, -100]}
+        heightClass="h-[calc(100vh+100px)]"
         viewBox="0 0 1440 400"
       />
-      {/* Layer 1: Closest, darkest silhouetted mountains */}
+      {/* Layer 2: Mid-Ground Mountains */}
       <MountainLayer
-        d="M0,400 L0,250 L100,320 L250,220 L400,340 L580,180 L720,300 L850,200 L1000,330 L1150,230 L1300,310 L1440,260 L1440,400 Z"
-        fillClass="fill-neutral-950" // Almost black silhouette
+        d="M0,400 L0,300 C150,330 300,280 450,310 C600,340 700,290 800,320 C900,350 1050,300 1200,330 L1350,290 L1440,320 L1440,400 Z"
+        fillClass="fill-neutral-800"
         opacityClass="opacity-100"
-        yOffsetRange={[0, -200]}
-        heightClass="h-[calc(100vh+200px)]"
+        yOffsetRange={[0, -150]}
+        heightClass="h-[calc(100vh+150px)]"
+        viewBox="0 0 1440 400"
+      />
+      {/* Layer 1: Foreground Mountains (Closest) */}
+      <MountainLayer
+        d="M-5,400 L-5,320 C100,350 200,280 350,340 C500,400 580,300 720,350 C850,400 950,310 1100,360 C1250,410 1350,320 1445,370 L1445,400 Z"
+        fillClass="fill-black"
+        opacityClass="opacity-100"
+        yOffsetRange={[0, -220]}
+        heightClass="h-[calc(100vh+220px)]"
         viewBox="0 0 1440 400"
       />
     </div>
